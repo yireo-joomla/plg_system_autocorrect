ce8f72cc56051245b61d2c1bafa1a442b1f32148
Jisse Reitsma <info@yireo.com>
2014-12-23 10:28:25 +0100

    New filter to remove trailing dots from title

---
+++ source/plugins/system/autocorrect/filters/nodots.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Joomla! plugin to autocorrect content
+ *
+ * @author Yireo (info@yireo.com)
+ * @copyright Copyright 2014
+ * @license GNU Public License
+ * @link http://www.yireo.com
+ * @contributor Jisse Reitsma, Yireo (main code)
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die( 'Restricted access' );
+
+// Import dependancies
+include_once __DIR__.'/../interface.php';
+
+/**
+ * Filtering class for removing dots at the end of lines
+ */
+class plgSystemAutocorrectFilterNodots implements plgSystemAutoCorrectFilterInterface
+{
+    public function filter($string)
+    {
+        $string = preg_replace('/\.$/', '', $string);
+
+        return $string;
+    }
+}
